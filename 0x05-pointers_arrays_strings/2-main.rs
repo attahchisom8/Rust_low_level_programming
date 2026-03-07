afadb34c45259480a196a6487f8624696e195eda
attahchisom8 <attahchisom8@gmail.com>
2026-03-07 20:07:57 +0000

    creating strlen witgmh smart pointer

---
+++ 0x05-pointers_arrays_strings/2-main.rs
@@ -0,0 +1,21 @@
+mod strlen_2;
+use strlen_2::_strlen;
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+fn main()
+{
+    let str;
+<<<<<<< HEAD
+    let len: u64;
+=======
+    len len: u64;
+>>>>>>> 68f3f77825520e950ae323bd87e78e8a605dbfad
+
+    str = "My first strlen!";
+    len = _strlen(str);
+    println!("{}", len);
+}
